88bf9b1fe6e6e0a9e29345b08427b860f790c2af
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2023-04-15 01:55:24 +0600

    update

--- pages/services/service.js
+++ pages/services/service.js
@@ -13,3 +13,11 @@ var fromDownToTop_s = document.querySelectorAll('.fromDownToTop')
 fromDownToTop_s.forEach(item=>{
     new ActivateInView(item)
 })
+
+
+var line_container = document.querySelector('.line_container');
+
+new ActivateInView(line_container)
+
+
+var line_items = document.querySelectorAll('.line_item')
